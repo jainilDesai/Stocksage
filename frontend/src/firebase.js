e09a5a3d5b540ff44b86e33f2e32bd3d9b14ca9a
Jainil9657 <desaijainil9657@gmail.com>
2025-03-24 21:48:28 +0530

    some issues fixed

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,7 +1,6 @@
 // src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
 const firebaseConfig = {
   apiKey: YOUR_API_KEY,
   authDomain: YOUR_AUTH_DOMAIN,
