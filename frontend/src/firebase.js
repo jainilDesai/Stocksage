0a7e4f214fe2223a8af76066832f4bb7b6d2599c
Jainil9657 <desaijainil9657@gmail.com>
2025-03-30 12:00:39 +0530

    .env added 2

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,13 +2,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: process.env.YOUR_API_KEY,
-  authDomain: process.env.YOUR_AUTH_DOMAIN,
-  projectId: process.env.YOUR_PROJECT_ID,
-  storageBucket: process.env.YOUR_STORAGE_BUCKET,
-  messagingSenderId: process.env.YOUR_MESSAGING_SENDER_ID,
-  appId: process.env.YOUR_APP_ID,
-  measurementId: process.env.YOUR_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_YOUR_API_KEY,
+  authDomain: import.meta.env.VITE_YOUR_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_YOUR_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_YOUR_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_YOUR_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_YOUR_APP_ID,
+  measurementId: import.meta.env.VITE_YOUR_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
